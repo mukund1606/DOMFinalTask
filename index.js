c094a651e74f54d34baf7882abd9eee405ef386f
Mukund Mittal <mukund.mukulmittal@gmail.com>
2023-02-27 18:58:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const myButton = document.querySelector("button");
+
+myButton.addEventListener("click", function () {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var player1 = document.getElementsByClassName("img1")[0];
+  var player2 = document.getElementsByClassName("img2")[0];
+  player1.setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+  player2.setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Won";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Won 🚩";
+  } else {
+    document.querySelector("h1").innerHTML = "It's a Draw";
+  }
+});
